5bee498e5ffd287641746ffd03fdd3f4dd8029ec
Jonathan Silva <jonathan.jgs@gmail.com>
2022-10-29 11:44:42 -0300

    Initial Commit

---
+++ TemperatureMonitor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TemperatureMonitor
+//
+//  Created by Jonathan Gonçalves Da Silva on 29/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemperatureMonitor/TemperatureMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureMonitorApp.swift
+//  TemperatureMonitor
+//
+//  Created by Jonathan Gonçalves Da Silva on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
